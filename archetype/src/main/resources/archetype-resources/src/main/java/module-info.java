cf891fe8e448276b9da883b374bd22d8a427b1e9
Marcel Witte <marcel@aliquis.de>
2024-12-18 23:27:25 +0100

    removed old dependency to (now non-existing) module cloud.quinimbus.rest

--- archetype/src/main/resources/archetype-resources/src/main/java/module-info.java
+++ archetype/src/main/resources/archetype-resources/src/main/java/module-info.java
@@ -6,7 +6,6 @@ open module $package {
     requires cloud.quinimbus.persistence.cdi;
     requires cloud.quinimbus.persistence.repositories;
     requires cloud.quinimbus.persistence.storage.mongo;
-    requires cloud.quinimbus.rest;
     requires cloud.quinimbus.rest.crud;
     requires cloud.quinimbus.tools;
     requires com.fasterxml.jackson.annotation;
